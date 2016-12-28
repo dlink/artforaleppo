eeba79eb45e98e33a3f807d263ad08d2d1963523
David Link <dvlink@gmail.com>
2016-12-28 23:15:10 +0000

    adding images to main page

--- web/index.py
+++ web/index.py
@@ -5,20 +5,36 @@ from vweb.html import *
 
 class Index(HtmlPage):
     
+    def __init__(self):
+        name = 'Art for Aleppo'
+        HtmlPage.__init__(self, name, include_form_tag=0)
+        self.style_sheets = [
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/' \
+                'bootstrap.min.css',
+            'css/index.css']
+
     def getHtmlContent(self):
-        return \
+        o = \
             self.getHeader() + \
             self.getBody() + \
             self.getFooter()
+        return div(o, class_='container body')
 
     def getHeader(self):
-        return p('header')
+        o = nav(
+            ul(li('Art for Aleppo') + \
+               li('Save the Children Syria') + \
+               li('Call to Artists') + \
+               li('Artwork Gallery') + \
+               li('Make a Donation')))
+        return div(o, class_='header')
 
     def getBody(self):
-        return p('body')
+        return img(src='images/artforaleppo.jpg')
 
     def getFooter(self):
-        return p('footer')
+        o = ''
+        return div(o, class_='footer')
 
 if __name__ == '__main__':
     Index().go()
