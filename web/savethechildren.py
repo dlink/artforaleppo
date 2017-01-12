61ac49cb22ccd5145c1bb5a7fa4a807964e4e10c
David Link <dvlink@gmail.com>
2017-01-12 05:02:35 +0000

    finalizing save the children page with pdf

--- web/savethechildren.py
+++ web/savethechildren.py
@@ -4,6 +4,10 @@ from basepage import BasePage
 
 class SaveTheChildren(BasePage):
 
+    def __init__(self):
+        BasePage.__init__(self)
+        self.style_sheets.extend(['css/savethechildren.css'])
+
     def getBody(self):
         html =  open('html/savethechildren.html', 'r').read()
         return html
