b9faab59827506d0513f69415ab3b4ac5b0d7a8e
David Link <dvlink@gmail.com>
2017-01-14 22:18:28 +0000

    finalize call to artists page

--- web/calltoartists.py
+++ web/calltoartists.py
@@ -4,6 +4,10 @@ from basepage import BasePage
 
 class CallToArtists(BasePage):
 
+    def __init__(self):
+        BasePage.__init__(self)
+        self.style_sheets.extend(['css/calltoartists.css'])
+
     def getBody(self):
         html =  open('html/calltoartists.html', 'r').read()
         return html
